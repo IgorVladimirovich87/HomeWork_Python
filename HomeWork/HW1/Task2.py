d5459eb5520be8d214f7c8dedd2bdbabb798210d
Igor <Chertopiatov87@yandex.ru>
2022-12-18 12:12:43 +0300

    all Task

--- HomeWork/HW1/Task2.py
+++ HomeWork/HW1/Task2.py
@@ -1,12 +1,9 @@
  # 2. Напишите программу для проверки ложности утверждения
  # (W ⋀ Z) ⋁ ¬Y ⋁ (¬X ≡ ¬W) для всех значений предикат.
-
-print("w y z")
-for w in [False, True]:
-    for y in [False, True]:
-       for z in [False, True]:
-           if (not w or y or z) != (not w or y or z):
-               print('Выражение: ¬(W ⋁ Y ⋁ Z) = ¬W ⋀ ¬Y ⋀ ¬Z - False')
-    break
-else:
-    print('Выражение: ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z - True')
+print("x y z w")
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            for w in range(2):
+                if not ((w and z) or not y or (not x == (not w))):
+                    print(x, y, z, w)
